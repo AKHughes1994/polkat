9dfb7a6d78c4e2eeafcdf13e055a227ae684533b
AKHughes1994 <hughes1@ualberta.ca>
2023-12-06 16:56:48 -0700

    Added 1D RM Synthesis scripts and a little guide

---
+++ PointSourceRMSynth/example/example_script.sh
@@ -0,0 +1,3 @@
+python3 ../RMtools_1D/do_RMsynth_1D.py ../datafiles/SwiftJ1727_rmsynth_60218.48831018519.txt -S -v -p -o 4
+
+python3 ../RMtools_1D/do_RMclean_1D.py ../datafiles/SwiftJ1727_rmsynth_60218.48831018519.txt -v -p
