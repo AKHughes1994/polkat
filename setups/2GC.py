6d73c941e5a500be5937bc0f9e6ceab68989171f
AKHughes1994 <49698839+AKHughes1994@users.noreply.github.com>
2024-02-29 17:33:22 -0700

    Update 2GC.py

--- setups/2GC.py
+++ setups/2GC.py
@@ -237,7 +237,7 @@ def main():
             step['comment'] = 'Run wsclean, masked deconvolution of the CORRECTED_DATA (self-calibrated) for {}'.format(targetname)
             step['dependency'] = step_i - 1
             step['id'] = 'WSCMA'+code
-            step['slurm_config'] = cfg.SLURM_HIGHMEM
+            step['slurm_config'] = cfg.SLURM_WSCLEAN
             step['pbs_config'] = cfg.PBS_WSCLEAN
             absmem = gen.absmem_helper(step,INFRASTRUCTURE,cfg.WSC_ABSMEM)
             syscall = CONTAINER_RUNNER+WSCLEAN_CONTAINER+' ' if USE_SINGULARITY else ''
